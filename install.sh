e68f1272a59842ec0e47734e7f4b631603302254
charlotte <33139256+charlottesgit@users.noreply.github.com>
2025-11-07 07:37:23 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 $!/bin/bash
 
+cp ${PWD}/.torproxy ${SUDO_HOME}/
+
 echo 'Defaults env_keep += "*_proxy *_PROXY"' > /etc/sudoers.d/05_proxy
 
 echo "source ~/.torproxy" >> ${SUDO_HOME}/.bashrc
 
-source ${SUDO_HOME}/.bashrc
+${SUDO_HOME}/.bashrc
 
 rm -- "$0"
