e7d805277c030f127f07388490929d466f54ccab
charlotte <33139256+charlottesgit@users.noreply.github.com>
2025-11-07 07:03:02 -0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+$!/bin/bash
+
+echo 'Defaults env_keep += "*_proxy *_PROXY"' > /etc/sudoers.d/05_proxy
+
+echo "source ~/.torproxy" > ${SUDO_HOME}/.bashrc
+
+source ${SUDO_HOME}/.bashrc
+
+rm -- "$0"
